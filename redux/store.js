3ad87dbb36613a5d596c6635ddaf3fd98b20b3e6
Lakesman Olalekan <102413469+lakesman19@users.noreply.github.com>
2023-12-11 08:00:44 +0100

    updated

--- redux/store.js
+++ redux/store.js
@@ -1,57 +1,44 @@
-const isClient = typeof window !== 'undefined'
+'use client'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { apiSlice } from './apiSlice'
+import authSlice from '@/user/authSlice'
 
-let store
-let persistor
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-if (isClient) {
-  const { configureStore, combineReducers } = require('@reduxjs/toolkit')
-  const {
-    FLUSH,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-    REHYDRATE,
-    persistReducer,
-    persistStore,
-  } = require('redux-persist')
-  const storage = require('redux-persist/lib/storage').default
-  const { apiSlice } = require('./apiSlice')
-  const authSlice = require('@/user/authSlice')
-
-  const rootReducers = combineReducers({
-    auth: authSlice,
-  })
-
-  const persistConfig = {
-    key: 'root',
-    storage: typeof window !== 'undefined' ? storage : null,
-  }
-
-  const persistedReducer = persistReducer(persistConfig, rootReducers)
-
-  store = configureStore({
-    reducer: {
-      [apiSlice.reducerPath]: apiSlice.reducer,
-      persistedReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }).concat(apiSlice.middleware),
-    devTools: true,
-  })
-
-  persistor = persistStore(store)
-} else {
-
-  // Define your server-side store configuration here if needed
+const rootReducers = combineReducers({
+  auth: authSlice,
+})
+const persistConfig = {
+  key: 'root',
+  storage:  typeof window !== 'undefined' ? storage:null
+  
 }
+const persistedReducer = persistReducer(persistConfig, rootReducers)
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
 
-export { store, persistor }
-
+  devTools: true,
+})
+export const persistor = persistStore(store)
 // import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
 
 // const createNoopStorage = () => {
@@ -69,43 +56,3 @@ export { store, persistor }
 // }
 // const storagee =
 //   typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage
-// 'use client'
-// import { configureStore, combineReducers } from '@reduxjs/toolkit'
-// import { apiSlice } from './apiSlice'
-// import authSlice from '@/user/authSlice'
-
-// import {
-//   FLUSH,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-//   REHYDRATE,
-//   persistReducer,
-//   persistStore,
-// } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
-
-// const rootReducers = combineReducers({
-//   auth: authSlice,
-// })
-// const persistConfig = {
-//   key: 'root',
-//   storage: typeof window !== 'undefined' ? storage : null,
-// }
-// const persistedReducer = persistReducer(persistConfig, rootReducers)
-// export const store = configureStore({
-//   reducer: {
-//     [apiSlice.reducerPath]: apiSlice.reducer,
-//     persistedReducer,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }).concat(apiSlice.middleware),
-
-//   devTools: true,
-// })
-// export const persistor = persistStore(store)
