282c1e7d34ccf4fc9c9e6edd59e2d70638de15fb
Lakesman Olalekan <102413469+lakesman19@users.noreply.github.com>
2023-12-24 11:06:22 +0100

    updated

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -46,7 +46,7 @@ const Modal = ({ isOpen, close, children, height }) => {
                                 leaveFrom='opacity-100 translate-y-0 sm:scale-100'
                                 leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
                             >
-                                <Dialog.Panel className={`relative transform overflow-y-scroll rounded-lg bg-[#FAFAFA] text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg p-4 flex flex-col gap-y-2 h-[${height}] w-[100%]`}>
+                                <Dialog.Panel className={`relative transform overflow-y-scroll rounded-lg bg-[#FAFAFA] text-left shadow-xl transition-all sm:my-8 sm:w-full max-w-2xl p-4 flex flex-col gap-y-2 h-[${height}] w-[100%]`}>
                                     {children}
                                 </Dialog.Panel>
                             </Transition.Child>
